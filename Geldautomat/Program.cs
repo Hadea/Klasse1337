c5b1b5e85485f475ad33d2052e784f7f50a856d0
Lutz Kadoch <LaMbdA2@gmx.at>
2021-04-26 14:53:52 +0200

    Added comment and function for greeting, added test for card

--- Geldautomat/Program.cs
+++ Geldautomat/Program.cs
@@ -4,9 +4,29 @@ namespace Geldautomat
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            printGreeting();
+            string cardID;
+            cardID = Console.ReadLine();
+
+            if (cardID[0] != 'K')
+            {
+                Console.WriteLine("Karte wird eingezogen weil ungültig");
+                return;
+            }
+
+
+
+        }
+
+        /// <summary>
+        /// Prints a greeting and asks for customer card
+        /// </summary>
+        private static void printGreeting()
+        {
+            Console.WriteLine("Hallo, ich bin ein Geldautomat");
+            Console.WriteLine("Bitte Karte einschieben");
         }
     }
 }
