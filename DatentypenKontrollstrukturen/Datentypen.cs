17a5bd278555c97397252dbdb592013ec7a0e263
Lutz Kadoch <LaMbdA2@gmx.at>
2021-04-27 11:40:03 +0200

    Added examples for loops and branches

---
+++ DatentypenKontrollstrukturen/Datentypen.cs
@@ -0,0 +1,34 @@
+﻿namespace DatentypenKontrollstrukturen
+{
+    class Datentypen
+    {
+        // Ganze Zahlen
+
+        byte Ganzzahl8BitOhneVorzeichen; // 0 bis 255
+        sbyte Ganzzahl8Bit; // -128  bis +127
+
+        short Ganzzahl16Bit; // -/+ 32 Tausend
+        ushort Ganzzahl16BitOhneVorzeichen; // 0 65535
+
+        int Ganzzahl32Bit; // -2,14 Milliarden +2,14 Milliarden
+        uint Ganzzahl32BitOhneVorzeichen; // 0 +4,29 Milliarden
+
+        long Ganzzahl64Bit; // +/- 9 Trillionen?
+        ulong Ganzzahl64BitOhneVorzeichen; // 0 bis 18 Trillionen?
+
+        // Angenäherte Zahlen / Gebrochene Zahlen
+
+        float Fliesskomma32Bit; // 5 bis 7 stellen genau
+        double Flieskomma64Bit; // 13 Stellen genau
+        decimal Fliesskomma128Bit; // braucht man fast nicht, VORSICHT SQL kennt das als Ganzzahl
+
+        // Zeichen
+
+        string Text; // Text/Zeichenkette, 2Byte pro Zeichen
+        char Buchstabe; // ein einzelnes Zeichen, 2 Byte
+
+        // Bit
+        bool EinzelnesBit; // nur true oder false
+
+    }
+}
---
+++ DatentypenKontrollstrukturen/Kontrollstrukturen.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DatentypenKontrollstrukturen
+{
+    class Kontrollstrukturen
+    {
+        public void Beispiele()
+        {
+
+            if (5 < 3) // testet ob eine aussage wahr ist
+            {
+                // wenn sie wahr ist
+            }
+
+            if (5 < 3) // testet ob eine aussage wahr ist
+            {
+                // wenn sie wahr ist
+            }
+            else
+            {
+                // wenn sie nicht wahr ist
+            }
+
+            if (3 > 7)
+                System.Console.WriteLine("Stimmt");
+            else
+                Console.WriteLine("Stimmt nicht");
+
+            if (5 == 7)
+            {
+                // wenn wahr
+            }
+            else if (5 > 3)
+            {
+                // wenn erstes nicht wahr und zweites wahr ergibt
+            }
+            else
+            {
+                // wenn keiner der tests wahr ergibt
+            }
+
+            if (8 < 7) throw new ArgumentOutOfRangeException(); // häufig am anfang einer funktion um die Parameter zu überprüfen
+
+            /////////////////////////////////////////////////////////////
+
+            int counter = 5;
+
+            while (counter > 0) // läuft solang die bedingung wahr ist, test vor dem ersten durchlauf
+            {
+                counter--;
+            }
+            Console.WriteLine(counter);
+
+            counter = 5;
+            do // testet erst nach dem ersten durchlauf *Spongebob meme* "Wanna see me do it again?"
+            {
+                Console.WriteLine("Bin in der schleife");
+                counter--;
+            } while (counter > 0);
+
+            /////////////////////////////////////////////////
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Bin in der For und i ist " + i);
+            }
+
+            for(;;) // Zoidberg FOR, variablenerstellung, test und zähleränderung sind optional
+            {
+                break; // beendet das For weil wir sonst hier eine endlosschleife haben
+            }
+
+            List<int> ZahlenListe = new();
+
+            foreach (int item in ZahlenListe) // geht einen Container von anfang bis ende durch
+            {
+                Console.WriteLine(item);
+            }
+
+            ///////////////////////////////////////////////////////
+
+            switch (counter)// schaut in die Variable und springt in den passenden case
+            {
+                default: // wenn kein case trifft wird default gemacht (optional), bei enum besser mitnehmen und mit exception füllen
+                    Console.WriteLine("Bin im Default gelandet");
+                    break;
+                case 3: // wenn in der variable vom switch eine 3 gefunden wird
+                    Console.WriteLine("Hab ne 3");
+                    break;// nach jedem case wird der switch beendet. (break, return, throw)
+                case 7: // wenn 7 oder 1 gefunden wird und die gleich behandelt werden sollen
+                case 1:
+                    Console.WriteLine("Da ist eine eins oder sieben drin");
+                    break;
+            }
+        }
+
+    }
+}
