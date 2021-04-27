1de926fc2fc5d518134626c01030092845549b3c
Lutz Kadoch <LaMbdA2@gmx.at>
2021-04-27 14:39:07 +0200

    added lotto challenge

---
+++ Lotto/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Lotto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rndGen = new Random();
+            for (int count = 0; count < 5; count++)
+            {
+                Console.WriteLine( rndGen.Next(1, 51)); 
+            }
+
+
+            //1. 5 zahlen zwischen 1 und 50 (inclusive) ausgeben, ohne doppelungen
+            //2. 2 zahlen zwischen 1 und 10 (inclusive) ausgeben, ohne doppelungen
+        }
+    }
+}
