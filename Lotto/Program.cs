490ee41685378395f367f59d9533b9d23099f609
Lutz Kadoch <LaMbdA2@gmx.at>
2021-04-27 16:54:17 +0200

    added comparison

--- Lotto/Program.cs
+++ Lotto/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lotto
 {
@@ -6,15 +7,39 @@ namespace Lotto
     {
         static void Main(string[] args)
         {
-            Random rndGen = new Random();
-            for (int count = 0; count < 5; count++)
-            {
-                Console.WriteLine( rndGen.Next(1, 51)); 
-            }
+            List<byte> Ziehung = new();
+            List<byte> SpielZettel = new() { 5, 12, 23, 42, 49 };
 
+            Random rndGen = new();
 
             //1. 5 zahlen zwischen 1 und 50 (inclusive) ausgeben, ohne doppelungen
-            //2. 2 zahlen zwischen 1 und 10 (inclusive) ausgeben, ohne doppelungen
+            do
+            {
+                byte newNumber = (byte)rndGen.Next(1, 51);
+                if (!Ziehung.Contains(newNumber))
+                    Ziehung.Add(newNumber);
+            } while (Ziehung.Count < 5);
+
+            Console.Write("Zahlen des Spielers: ");
+            foreach (byte item in SpielZettel)
+                Console.Write(item + " ");
+
+            Console.Write("\nZahlen der Ziehung:  ");
+            foreach (byte item in Ziehung)
+                Console.Write(item + " ");
+
+
+            int treffer = 0;
+            foreach (byte item in SpielZettel)
+            {
+                if (Ziehung.Contains(item))
+                {
+                    treffer++;
+                }
+            }
+
+            Console.WriteLine("\nAnzahl der richtigen: " + treffer);
+
         }
     }
 }
