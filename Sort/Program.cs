8f479d6f5a6df4bcdd55460c34926311cadd886c
Lutz Kadoch <LaMbdA2@gmx.at>
2021-04-28 15:23:03 +0200

    finished naiive selection sort

--- Sort/Program.cs
+++ Sort/Program.cs
@@ -7,23 +7,44 @@ namespace Sort
     {
         static void Main(string[] args)
         {
-            List<byte> ArrayToSort = new() {7,5,2,3,9,1,8 };
-            for (int counter = 0; counter < ArrayToSort.Count; counter++)
+            List<byte> ArrayToSort = new() { 7, 5, 2, 3, 9, 1, 8 };
+
+            for (byte counter = 0; counter < ArrayToSort.Count; counter++)
             {
-                Console.Write( ArrayToSort[counter] + " " );
+                Console.Write(ArrayToSort[counter] + " ");
             }
+
+            int sum = 0;
+            // summe aller element des arrays
+            foreach (var item in ArrayToSort)
+            {
+                sum += item; // entspricht sum = sum + item
+            }
+            Console.WriteLine("\nDie summe aller elemente ist: " + sum + "\n");
+
+
 
             // sortieren
 
-            // alle elemente des arrays durchgehen
-            //    alle verbleibenden elemente des array durchgehen
-            //        wenn element an äusserem zähler grösser als element an innerem zähler
-            //            backup erstellen vom inneren wert
-            //            inneren wert mit äusserem überschreiben
-            //            äusseren wert mit backup überschreiben
-            //        ende wenn
-            //    ende array durchgehen
-            // ende array durchgehen
+            for (int outer = 0; outer < ArrayToSort.Count; outer++) // alle elemente des arrays durchgehen
+            {
+                for (int inner = outer + 1; inner < ArrayToSort.Count; inner++)//alle verbleibenden elemente des array durchgehen
+                {
+                    if (ArrayToSort[outer] > ArrayToSort[inner])//wenn element an äusserem zähler grösser als element an innerem zähler
+                    {
+                        byte backup = ArrayToSort[outer];//backup erstellen vom äusseren wert
+                        ArrayToSort[outer] = ArrayToSort[inner];//äusseren wert mit innerem überschreiben
+                        ArrayToSort[inner] = backup;//inneren wert mit backup überschreiben
+                    }//ende wenn
+                }//ende array durchgehen
+            } // ende array durchgehen
+
+
+            // sortiertes array ausgeben
+            foreach (var item in ArrayToSort)
+            {
+                Console.Write(item + " ");
+            }
 
             // nochmal ausgeben
 
