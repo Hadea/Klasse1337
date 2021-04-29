3af3d29d172827f24fbb48f4d7740bec88a43b7e
Lutz Kadoch <LaMbdA2@gmx.at>
2021-04-29 15:33:25 +0200

    added pin check and simple money query

--- Geldautomat/Program.cs
+++ Geldautomat/Program.cs
@@ -4,12 +4,13 @@ namespace Geldautomat
 {
     class Program
     {
+
         static void Main()
         {
-            Console.WriteLine("Hallo, ich bin ein Geldautomat");
-            Console.WriteLine("Bitte Karte einschieben");
+            PrintGreeting();
+
             string cardID;
-            cardID = Console.ReadLine();
+            cardID = Console.ReadLine(); // entspricht std::cin , liesst von der konsole bis zum enter
 
             if (cardID[0] != 'K')
             {
@@ -17,13 +18,40 @@ namespace Geldautomat
                 return;
             }
 
-            // bis zu 3x nach PIN fragen, wenn dann noch immer falsch -> sperren und ende
 
-            // fragen wieviel geld der nutzer haben möchte
+            Console.WriteLine("Bitte Pin eingeben:");
+            bool LoginStatus = false;
+            byte TriesRemaining = 3;
+            do
+            {
+                string pin = Console.ReadLine();
+                if (pin == "1337")
+                {
+                    LoginStatus = true;
+                    break;
+                }
+                Console.WriteLine("Pin falsch!");
 
-            // geld ausgeben (writeline genügt)
-            // karte ausgeben (writeline genügt)
-            // geld abbuchen (writeline genügt)
+            } while (--TriesRemaining > 0);
+
+            if (LoginStatus == false)
+            {
+                Console.WriteLine("3x Falscher Pin. Karte eingezogen");
+                return;
+            }
+
+            Console.WriteLine("Wieviel Geld möchten sie abheben?");
+            string Abhebebetrag = Console.ReadLine();
+
+            Console.WriteLine("Ihr Geld: " + Abhebebetrag + " wird ausgeworfen");
+            Console.WriteLine("Ihre Karte wird ausgeworfen");
+            Console.WriteLine("Geld abgebucht");
+        }
+
+        private static void PrintGreeting()
+        {
+            Console.WriteLine("Hallo, ich bin ein Geldautomat");
+            Console.WriteLine("Bitte Karte einschieben");
         }
     }
 }
