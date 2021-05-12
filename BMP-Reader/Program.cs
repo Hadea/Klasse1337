e135b3cb82785f0cb99b0071c77b060cbe217e53
Lutz <lambda2@gmx.at>
2021-05-12 12:23:38 +0200

    added bitmap challenge

---
+++ BMP-Reader/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BMP_Reader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // BMP-Datei mit dem BinaryReader lesen und Dateigrösse auf die Konsole ausgeben
+
+            // Bonus1: Auflösung der Datei ausgeben
+            // Bonus2: Bit pro pixel ausgeben
+
+            // Bonus3: Klasse(n) schreiben welche diese Daten aufnehmen
+            // Bonus4: Klasse(n) schreiben welche .ToString() überschreiben sodass ein Console.Write zum ausgeben genügt
+
+
+        }
+    }
+}
