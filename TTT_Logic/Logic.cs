71282ec2685357d8e7b808e7e2b6b15d8ba7ea98
Lutz <lambda2@gmx.at>
2021-05-25 14:31:57 +0200

    Added tests, solved all tests

--- TTT_Logic/Logic.cs
+++ TTT_Logic/Logic.cs
@@ -4,7 +4,10 @@ namespace TTT_Logic
 {
     public class Logic
     {
-        private Board[,] mGameBoard = new Board[3,3];
+        private Board[,] mGameBoard = new Board[3, 3];
+        private bool mCurrentPlayer;
+        private bool mGameRunning = true;
+        private byte mTurnCounter = 0;
 
         public Board[,] GetGameBoard()
         {
@@ -14,10 +17,56 @@ namespace TTT_Logic
         public TurnResult PlayerTurn(int x, int y)
         {
             if (x > 2 || y > 2) return TurnResult.Invalid;
+            if (x < 0 || y < 0) return TurnResult.Invalid;
+            if (mGameBoard[y, x] != Board.Empty) return TurnResult.Invalid;
+            if (!mGameRunning) return TurnResult.Invalid;
+            if (mTurnCounter > 7) return TurnResult.Draw;
 
-            mGameBoard[y, x] = Board.O;
+            mGameBoard[y, x] = mCurrentPlayer ? Board.O : Board.X;
+            mTurnCounter++;
 
-            return TurnResult.Valid;
+            if (mCheckWin())
+            {
+                mGameRunning = false;
+                return mCurrentPlayer ? TurnResult.WinO : TurnResult.WinX;
+            }
+            else
+            {
+                mCurrentPlayer = !mCurrentPlayer;
+                return TurnResult.Valid;
+            }
+        }
+
+        private bool mCheckWin()
+        {
+            //horizontal
+            if (mGameBoard[0, 0] != Board.Empty && mGameBoard[0, 0] == mGameBoard[0, 1] && mGameBoard[0, 0] == mGameBoard[0, 2]) return true;
+            if (mGameBoard[1, 0] != Board.Empty && mGameBoard[1, 0] == mGameBoard[1, 1] && mGameBoard[1, 0] == mGameBoard[1, 2]) return true;
+            if (mGameBoard[2, 0] != Board.Empty && mGameBoard[2, 0] == mGameBoard[2, 1] && mGameBoard[2, 0] == mGameBoard[2, 2]) return true;
+
+            // vertikal
+            if (mGameBoard[0, 0] != Board.Empty && mGameBoard[0, 0] == mGameBoard[1, 0] && mGameBoard[0, 0] == mGameBoard[2, 0]) return true;
+            if (mGameBoard[0, 1] != Board.Empty && mGameBoard[0, 1] == mGameBoard[1, 1] && mGameBoard[0, 1] == mGameBoard[2, 1]) return true;
+            if (mGameBoard[0, 2] != Board.Empty && mGameBoard[0, 2] == mGameBoard[1, 2] && mGameBoard[0, 2] == mGameBoard[2, 2]) return true;
+
+            //diagonal
+            if (mGameBoard[0, 0] != Board.Empty && mGameBoard[0, 0] == mGameBoard[1, 1] && mGameBoard[0, 0] == mGameBoard[2, 2]) return true;
+            if (mGameBoard[0, 2] != Board.Empty && mGameBoard[0, 2] == mGameBoard[1, 1] && mGameBoard[0, 2] == mGameBoard[2, 0]) return true;
+
+            return false;
+        }
+
+        public bool GetCurrentPlayer()
+        {
+            return mCurrentPlayer;
+        }
+
+        public void Reset()
+        {
+            mGameRunning = true;
+            mCurrentPlayer = !mCurrentPlayer;
+            mGameBoard = new Board[3, 3];
+            mTurnCounter = 0;
         }
     }
 }
--- TTT_Tests/LogicTests.cs
+++ TTT_Tests/LogicTests.cs
@@ -4,6 +4,37 @@ using TTT_Logic;
 
 namespace TTT_Tests
 {
+    /* Checkliste für tests (unvollständig)
+     * Parameter einer Funktion
+     *      - Null
+     *      - Negative werte
+     *      - Leere Container
+     *      - Zu grosse werte
+     *      - korrekte exceptions wenn fehlerhafte werte benutzt werden
+     * 
+     * Rückgabe einer Funktion / out-Parameter
+     *      - Null
+     *      - Leere Container
+     *      - Erwarteter Wert
+     */
+
+    /* Nach Spielstart ein leeres Spielfeld
+     * Nach einem Zug auf eine gültige koordinate muss das gewählte feld befüllt sein, der rest muss weiterhin leer sein
+     * Ein Zug auf ein belegtes feld muss "Ungültig" liefern
+     * Ein Zug auf eine ungültige Koordinate (ausserhalb) muss "Ungültig" liefern
+     * Zwei aufeinanderfolgende Züge auf unterschiedliche gültige koordinaten müssen unterschiedliche steine beinhalten
+     * Wenn der letzte stein auf das feld gesetzt wird und keinen Sieg bedeutet muss "Draw" geliefert werden
+     * Wenn der letzte stein auf dem feld zu einer siegbedingung führt muss "sieg" zurückgegeben werden
+     * Wenn 3 gleiche steine in einer reihe sind soll "Sieg" zurückgegeben werden 
+     * Wenn 3 gleiche steine in einer spalte sind soll "Sieg" zurückgegeben werden 
+     * Wenn 3 gleiche steine in einer diagonalen sind soll "Sieg" zurückgegeben werden 
+     * Aufeinanderfolgende fehlerhafte züge führen nicht zum spielerwechsel
+     * 
+     * Ein Spieler darf nicht mehr als 10 mal in folge als erster am zug sein / Spieler sind immer abwechslnd dran/ verlierer beginnt
+     * Nach einem Sieg sind normalerweise gültige züge auch "Ungültig"
+     * Nach einem Sieg bleibt der aktuelle spieler auf dem Siegspieler stehen
+     */
+
     [TestClass]
     public class LogicTests
     {
@@ -18,7 +49,7 @@ namespace TTT_Tests
         public void EmptyFieldAfterCreation()
         {
             Logic l = new();
-            Board[,] returnedBoard= l.GetGameBoard();
+            Board[,] returnedBoard = l.GetGameBoard();
 
             foreach (Board item in returnedBoard)
             {
@@ -34,7 +65,7 @@ namespace TTT_Tests
 
             // ausführung
             TurnResult result = l.PlayerTurn(0, 0); // erster zug auf gültige koordinaten, muss valid ergeben
-            
+
             // überprüfen des ergebnisses
             Assert.IsTrue(result == TurnResult.Valid); // testen ob auch valid zurück kam
             Assert.IsTrue(l.GetGameBoard()[0, 0] != Board.Empty);// testen ob der Zug auch auf dem Spielfeld vermerkt ist
@@ -69,24 +100,26 @@ namespace TTT_Tests
                 Assert.IsTrue(item == Board.Empty);
             }
         }
-        
+
         [TestMethod]
         public void PlayerSwitch_ValidMove()
         {
             Logic l = new();
 
+            // playerturn nimmt erst X dann Y
             Assert.IsTrue(l.PlayerTurn(1, 1) == TurnResult.Valid);
             Assert.IsTrue(l.PlayerTurn(0, 0) == TurnResult.Valid);
             Assert.IsTrue(l.PlayerTurn(0, 1) == TurnResult.Valid);
 
             var returnedBoard = l.GetGameBoard();
 
+            // Arrayzugriff ist erst Y dann X
             Assert.IsTrue(returnedBoard[1, 1] != Board.Empty);
             Assert.IsTrue(returnedBoard[0, 0] != Board.Empty);
-            Assert.IsTrue(returnedBoard[0, 1] != Board.Empty);
+            Assert.IsTrue(returnedBoard[1, 0] != Board.Empty);
 
-            Assert.IsTrue(returnedBoard[1, 1] == returnedBoard[0,1]);
-        }        
+            Assert.IsTrue(returnedBoard[1, 1] == returnedBoard[1, 0]);
+        }
 
         [TestMethod]
         public void PlayerSwitch_InvalidMove()
@@ -102,7 +135,7 @@ namespace TTT_Tests
             Assert.IsTrue(returnedBoard[1, 1] != Board.Empty);
             Assert.IsTrue(returnedBoard[0, 0] != Board.Empty);
 
-            Assert.IsTrue(returnedBoard[0, 0] != returnedBoard[1,1]);
+            Assert.IsTrue(returnedBoard[0, 0] != returnedBoard[1, 1]);
         }
 
         [TestMethod]
@@ -120,8 +153,108 @@ namespace TTT_Tests
             Assert.IsTrue(turnResult == TurnResult.WinO || turnResult == TurnResult.WinX);//PlayerA gewinnt
         }
 
+        [TestMethod]
+        public void EndGame_Draw()
+        {
+            Logic l = new();
+            //X O X
+            //O X O 
+            //O X O
+
+            Assert.IsTrue(l.PlayerTurn(0, 1) == TurnResult.Valid); // O
+            Assert.IsTrue(l.PlayerTurn(0, 0) == TurnResult.Valid); // X
+            Assert.IsTrue(l.PlayerTurn(1, 0) == TurnResult.Valid); // O
+            Assert.IsTrue(l.PlayerTurn(0, 2) == TurnResult.Valid); // X
+            Assert.IsTrue(l.PlayerTurn(2, 0) == TurnResult.Valid); // O 
+            Assert.IsTrue(l.PlayerTurn(1, 1) == TurnResult.Valid); // X
+            Assert.IsTrue(l.PlayerTurn(1, 2) == TurnResult.Valid); // O
+            Assert.IsTrue(l.PlayerTurn(2, 1) == TurnResult.Valid); // X
+            Assert.IsTrue(l.PlayerTurn(2, 2) == TurnResult.Draw); // O
+        }
+
+        [TestMethod]
+        public void EndGame_PlayablaAfterDrawAndReset()
+        {
+            Logic l = new();
+            //X O X
+            //O X O 
+            //O X O
+
+            Assert.IsTrue(l.PlayerTurn(0, 1) == TurnResult.Valid); // O
+            Assert.IsTrue(l.PlayerTurn(0, 0) == TurnResult.Valid); // X
+            Assert.IsTrue(l.PlayerTurn(1, 0) == TurnResult.Valid); // O
+            Assert.IsTrue(l.PlayerTurn(0, 2) == TurnResult.Valid); // X
+            Assert.IsTrue(l.PlayerTurn(2, 0) == TurnResult.Valid); // O 
+            Assert.IsTrue(l.PlayerTurn(1, 1) == TurnResult.Valid); // X
+            Assert.IsTrue(l.PlayerTurn(1, 2) == TurnResult.Valid); // O
+            Assert.IsTrue(l.PlayerTurn(2, 1) == TurnResult.Valid); // X
+            Assert.IsTrue(l.PlayerTurn(2, 2) == TurnResult.Draw); // O
+
+            l.Reset();
+            Assert.IsTrue(l.PlayerTurn(0, 1) == TurnResult.Valid); // O
+
+        }
+
+        [TestMethod]
+        public void EndGame_NoMovesAfterWin()
+        {
+            Logic l = new();
+
+            Assert.IsTrue(l.PlayerTurn(0, 1) == TurnResult.Valid);//PlayerA
+            Assert.IsTrue(l.PlayerTurn(1, 1) == TurnResult.Valid);//PlayerB
+
+            Assert.IsTrue(l.PlayerTurn(0, 2) == TurnResult.Valid);//PlayerA
+            Assert.IsTrue(l.PlayerTurn(1, 2) == TurnResult.Valid);//PlayerB
+
+            var turnResult = l.PlayerTurn(0, 0);
+            Assert.IsTrue(turnResult == TurnResult.WinO || turnResult == TurnResult.WinX);//PlayerA gewinnt
+
+            Assert.IsTrue(l.PlayerTurn(2, 2) == TurnResult.Invalid);//Ungültig da das Spiel schon vorbei ist
+        }
         //TODO: win on last stone
-        //TODO: draw
-        //TODO: no moves after win
+
+        [TestMethod]
+        public void Player_GetCurrentPlayer()
+        {
+            Logic l = new();
+
+            bool result = l.GetCurrentPlayer();
+            l.PlayerTurn(0, 0);
+            Assert.IsTrue(result != l.GetCurrentPlayer());
+        }
+        [TestMethod]
+        public void Player_GetCurrentPlayer_InvalidMove()
+        {
+            Logic l = new();
+
+            bool result = l.GetCurrentPlayer();
+            l.PlayerTurn(0, 0); // X
+            l.PlayerTurn(0, 1); // O
+            l.PlayerTurn(0, 1); // X ungültig, x bleibt am zug
+            Assert.IsTrue(result == l.GetCurrentPlayer());
+        }
+
+        [TestMethod]
+        public void Reset_SwitchesPlayer()
+        {
+            Logic l = new();
+            bool firstPlayer = l.GetCurrentPlayer();
+
+            l.Reset();
+            Assert.IsTrue(firstPlayer != l.GetCurrentPlayer());
+            l.Reset();
+            Assert.IsTrue(firstPlayer == l.GetCurrentPlayer());
+        }
+
+        [TestMethod]
+        public void Reset_ClearsTheBoard()
+        {
+            Logic l = new();
+            l.PlayerTurn(1, 1);
+            Assert.IsTrue(l.GetGameBoard()[1, 1] != Board.Empty);
+            l.Reset();
+            Assert.IsTrue(l.GetGameBoard()[1, 1] == Board.Empty);
+      
+        }
     }
 }
