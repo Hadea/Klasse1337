fe33a299243f05384c145047082c0f8ae2325158
Lutz <lambda2@gmx.at>
2021-05-28 13:13:23 +0200

    added example for struct

---
+++ DatentypenKontrollstrukturen/Data.cs
@@ -0,0 +1,33 @@
+﻿
+namespace DatentypenKontrollstrukturen
+{
+    struct Data
+    {
+        public int NumberA;
+        public byte NumberB;
+        public string TextA;
+        void Addition( int c)
+        {
+
+        }
+        public Data(int a, byte b, string c)
+        {
+            NumberA = a;
+            NumberB = b;
+            TextA = c;
+        }
+    }
+
+    class Structexamples
+    {
+
+        public void DoSomething()
+        {
+            Data b;
+            b.NumberA = 4;
+            b.NumberB = 2;
+            b.TextA = "Hallo";
+        }
+
+    }
+}
